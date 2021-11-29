807f90599fd7d9fc6a4358883b6389c84c409023
SitNis <rus.lol2011@yandex.ru>
2021-11-29 16:17:01 +0300

    add instance method for user, add class method for test

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,5 @@
 class Test < ApplicationRecord
+  def self.tests_by(category)
+    Test.joins("JOIN categories ON categories.id = tests.category_id").where("categories.title = ?", category).order('id DESC')
+  end
 end
