061ed62906b090dd67afeb0047485e0866752b26
SitNis <rus.lol2011@yandex.ru>
2021-12-01 13:17:29 +0300

    Change test and user models

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,16 +1,17 @@
 class Test < ApplicationRecord
+
+  belongs_to :author, class_name: "User", foreign_key: 'author_id'
+  belongs_to :category
+
+  has_many :questions, dependent: :destroy
+  has_many :tests_users
+  has_many :users, through: :tests_users, dependent: :destroy
+
   def self.tests_by(category)
       joins(:category)
       .where(categories: { title: category })
       .order(title: :desc)
       .pluck(:title)
   end
-
-  belongs_to :author, class_name: "User", foreign_key: 'author_id'
-  belongs_to :category
-
-  has_many :questions, dependent: :destroy
-  has_many :tests_users, dependent: :destroy
-  has_many :users, through: :tests_users, dependent: :destroy
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
-  def show_tests_list_by(level)
-    Test.joins(:tests_users)
-    .where(level: level, tests_users: {user_id: id})
-  end
 
   has_many :created_test, class_name: "Test", foreign_key: "author_id", dependent: :destroy
   has_many :tests_users, dependent: :destroy
   has_many :tests, through: :tests_users, dependent: :destroy
 
+  def show_tests_list_by(level)
+    tests.where(level: level)
+  end
+  
 end
