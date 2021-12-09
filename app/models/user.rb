73393fcc16822f571aa7736b02eb3848d27081ae
SitNis <rus.lol2011@yandex.ru>
2021-12-09 15:54:51 +0300

    add user registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   has_many :test_passages, dependent: :destroy
   has_many :tests, through: :test_passages, dependent: :destroy
 
-  validates :name, presence: true
+  has_secure_password
 
   def show_tests_list_by(level)
     tests.where(level: level)
@@ -13,5 +13,5 @@ class User < ApplicationRecord
   def test_passage(test)
     test_passages.order(id: :desc).find_by(test_id: test.id)
   end
-  
+
 end
