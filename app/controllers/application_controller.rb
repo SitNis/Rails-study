72929958c1d853f801046cdb41e0ce59d459f946
SitNis <rus.lol2011@yandex.ru>
2021-12-09 23:42:32 +0300

    Fix authenticate_user! method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
 
   def authenticate_user!
     unless current_user
-      redirect_to tests_path
+      redirect_to login_path
     end
   end
 
