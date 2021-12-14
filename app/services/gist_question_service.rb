7fa85af61b1599ed9a1eeba3ea012a127ab2e2ad
SitNis <rus.lol2011@yandex.ru>
2021-12-14 22:33:13 +0300

    Init octokit and question service

---
+++ app/services/gist_question_service.rb
@@ -0,0 +1,34 @@
+class GistQuestionService
+
+  ACCESS_TOKEN = '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'
+
+  def initialize(question, client: nil)
+    @question = question
+    @test = @question.test
+    @client = client || Octokit::Client.new(:access_token => ACCESS_TOKEN)  
+  end
+
+  def call
+    @client.create_gist(gist_params)
+  end
+
+  private
+
+  def gist_params
+    {
+      description: "A question about #{@test.title} from TestGuru",
+      files: {
+        'test-guru-question.txt' => {
+          content: gist_content
+        }
+      }
+    }
+  end
+
+  def gist_content
+    content = [@question.title]
+    content += @question.answers.pluck(:title)
+    content.join("\n")
+  end
+
+end
