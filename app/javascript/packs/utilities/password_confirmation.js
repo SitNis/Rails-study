1b6b6a4b3aa807cd7e88928fc4457f6a4b7d9173
SitNis <rus.lol2011@yandex.ru>
2021-12-23 12:34:21 +0300

    check password field

--- app/javascript/packs/utilities/password_confirmation.js
+++ app/javascript/packs/utilities/password_confirmation.js
@@ -1,16 +1,22 @@
 document.addEventListener('turbolinks:load', function () {
-  var confirmation_password = document.querySelector('.confirmation-password')
+  const confirmation_password = document.querySelector('.confirmation-password')
+  const password = document.querySelector('.password')
   confirmation_password.addEventListener('keyup', checkPassword)
+  password.addEventListener('keyup', checkPassword)
 })
 
 function checkPassword() {
-  let password = document.querySelector('.password').querySelector('input')
-  let confirmation_password = document.querySelector('.confirmation-password').querySelector('input')
-  if (password.value === confirmation_password.value) {
+  const password = document.querySelector('.password').querySelector('input')
+  const confirmation_password = document.querySelector('.confirmation-password').querySelector('input')
+  if (password.value === confirmation_password.value){
     confirmation_password.classList.remove('invalid')
     confirmation_password.classList.add('valid')
-  } else {
+    password.classList.remove('invalid')
+    password.classList.add('valid')
+  } else if (confirmation_password.value != '') {
     confirmation_password.classList.remove('valid')
     confirmation_password.classList.add('invalid')
+    password.classList.remove('valid')
+    password.classList.add('invalid')
   }
 }
