60b94ba28fa6d3c34fef2f23761da121ed76c935
SitNis <rus.lol2011@yandex.ru>
2021-12-25 20:41:17 +0300

    delete spaces

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -1,4 +1,4 @@
 class Badge < ApplicationRecord
   has_many :users_badge, dependent: :destroy
-  has_many :users, through: :users_badge, dependent: :destroy 
+  has_many :users, through: :users_badge, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
   has_many :created_test, class_name: 'Test', foreign_key: 'author_id', dependent: :destroy
   has_many :test_passages, dependent: :destroy
   has_many :tests, through: :test_passages, dependent: :destroy
-  has_many :users_badge, dependent: :destroy 
+  has_many :users_badge, dependent: :destroy
   has_many :badges, through: :users_badge, dependent: :destroy
 
   def show_tests_list_by(level)
