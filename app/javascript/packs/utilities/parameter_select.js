a639d6faebb2787e243209f62cd45160eae6b3ce
SitNis <rus.lol2011@yandex.ru>
2022-02-20 21:37:51 +0300

    fix parameter select

--- app/javascript/packs/utilities/parameter_select.js
+++ app/javascript/packs/utilities/parameter_select.js
@@ -1,21 +1,24 @@
 document.addEventListener('turbolinks:load', function() {
   const rule_type = document.getElementById('badge_rule_type')
+  const levelSelector = document.querySelector('.level-selector')
+  const categorySelector = document.querySelector('.category-selector')
   if (rule_type) {
     rule_type.addEventListener("change", function () {
-      rule_parameter_vision(rule_type.value)
+      rule_parameter_vision(rule_type.value, levelSelector, categorySelector)
     })
   }
 })
 
-function rule_parameter_vision(value){
-  const levelSelector = document.querySelector('.level-selector')
-  const categorySelector = document.querySelector('.category-selector')
+function rule_parameter_vision(value, levelSelector, categorySelector){
+  const rule_parameter_selector = document.querySelector('.parameter-selector')
   if (value == "Level") {
+    rule_parameter_selector.appendChild(levelSelector)
     levelSelector.classList.remove('hide')
-    categorySelector.classList.add('hide')
+    rule_parameter_selector.removeChild(categorySelector)
   } else if (value == "Category") {
-    levelSelector.classList.add('hide')
+    rule_parameter_selector.appendChild(categorySelector)
     categorySelector.classList.remove('hide')
+    rule_parameter_selector.removeChild(levelSelector)
   } else {
     levelSelector.classList.add('hide')
     categorySelector.classList.add('hide')
